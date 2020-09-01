ef19e28ac772b81674c0886cd775ca86e396a017
Bhaswanth53 <bhaswanth22@gmail.com>
2020-09-01 11:45:41 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,51 @@
+var header = $(".cus-nav")
+var width = $( window ).width();
+
+if(width > 768) {
+    $('#fullpage').pagepiling({
+        menu: "#menu",
+        direction: 'vertical',
+        verticalCentered: true,
+        sectionsColor: [],
+        anchors: [],
+        scrollingSpeed: 700,
+        easing: 'swing',
+        loopBottom: false,
+        loopTop: false,
+        css3: true,
+        navigation: {
+            'textColor': '#000',
+            'bulletsColor': '#000',
+            'position': 'right',
+            'tooltips': ['section1', 'section2', 'section3', 'section4']
+        },
+        normalScrollElements: null,
+        normalScrollElementTouchThreshold: 5,
+        touchSensitivity: 5,
+        keyboardScrolling: true,
+        sectionSelector: '.section',
+        animateAnchor: false,
+
+        //events
+        onLeave: function(index, nextIndex, direction){},
+        afterLoad: function(anchorLink, index){},
+        afterRender: function(){},
+    });
+} else {
+    $('#fullpage').pagepiling()
+}
+
+$(window).scroll(function() {    
+    var scroll = $(window).scrollTop();
+    if (scroll >= 200) {
+            header.addClass("fixed");
+    } else {
+            header.removeClass("fixed");
+    }
+});
+
+$(document).on('click', 'a.link', function() {
+    event.preventDefault()
+    var section = $(this).attr('data-move')
+    $('#fullpage').pagepiling.moveTo(section);
+})
